243377d1961e0cecdc60945b68ebf6cfbe581bf9
Nad A.A <toku2288@protonmail.com>
2021-08-13 01:19:49 +0800

    Skeleton project
    - Make a new Q2_4.26 project
    - Add VRExpPlugin
    - Import Input bindings and custom collision channels
    - Configure Android Packaging
    - MIGRATE the tags first!!
    - Then migrated Steam_VR_GM (And all assets under it, which includes the VR pawn and hands)
    
    We now have a skeleton project with the VRExpPlugin + basic VR pawn built out from the plugin classes!

---
+++ Source/Rayscape/RayscapeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RayscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RAYSCAPE_API ARayscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
